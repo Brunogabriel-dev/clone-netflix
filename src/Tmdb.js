fe5c4b5fdb42dadba39ed0a4911b349d0167e119
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-02-11 13:05:43 -0300

    Inclusão de texto

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -13,42 +13,42 @@ export default {
       {
         slug: 'originals',
         title: 'Originais do Netflix',
-        items: []
+        items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'trending',
         title: 'Recomendados para Você',
-        items: []
+        items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'toprated',
         title: 'Em Alta',
-        items: []
+        items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'action',
         title: 'Ação',
-        items: []
+        items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'comedy',
         title: 'Comédia',
-        items: []
+        items:  await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'horror',
         title: 'Terror',
-        items: []
+        items:  await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'romance',
         title: 'Romance',
-        items: []
+        items:  await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'documentary',
         title: 'Documentários',
-        items: []
+        items:  await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
       },
     ];
   }
