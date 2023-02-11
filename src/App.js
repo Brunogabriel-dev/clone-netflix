3bde21f3f2cb30fe3040d90ddcba620f8ce19c93
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-02-11 13:22:01 -0300

    Inclusão de texto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Tmdb from './Tmdb';
 
 export default () => {
+
+  useEffect(() => {
+    const loadAll = async () => {
+      
+      let list = await Tmdb.getHomeList();
+      console.log(list);
+
+    }
+
+    loadAll();
+  }, []);
+
   return (
     <div>
        Óla Mundo!
