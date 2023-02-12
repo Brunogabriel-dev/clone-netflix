9a6868fe3c179c08bda6a0b265921c620d006edc
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-02-12 13:25:09 -0300

    Inclusão de texto

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -5,6 +5,10 @@ export default ({item}) => {
   console.log(item);
 
   let firstDate = new Date(item.first_air_date);
+  let genres = [];
+  for(let i in item.genres) {
+    genres.push(item.genres[i].name );
+  }
 
   return (
     <section className="featured" style={{
@@ -22,9 +26,10 @@ export default ({item}) => {
           </div>
           <div className="featured--descriptio">{item.overview}</div>
           <div className="featured--buttons">
-
+            <a href={`/watch/${item.id}`}>▶ Assistir</a>
+            <a href={`/list/add/${item.id}`}>+ Minha Lista</a>
           </div>
-          <div className="featured--genres"><strong>Gêneros:</strong> ...</div>
+          <div className="featured--genres"><strong>Gêneros:</strong> {genres.join(', ')}</div>
         </div>
       </div>
     </section>
