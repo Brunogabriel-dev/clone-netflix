83c2dd6bc597dcc53ed2ac1dc2c84f80ad0183e4
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-02-12 09:01:34 -0300

    Inclusão de texto

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -3,16 +3,15 @@ import './MovieRow.css';
 
 export default ({title, items}) => {
   return (
-    <div>
+    <div className="movieRow">
       <h2>{title}</h2>
       <div className="movieRow--listarea">
         <div className="movieRow--list">
         {items.results.length > 0 && items.results.map((item, key)=>(
-          <div className="movieRow--item">
+          <div key={key} className="movieRow--item">
             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_titlr} />
           </div>
-       ))}
-
+        ))}
         </div>
       </div>
     </div>
