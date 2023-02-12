07089bec5d71d9fbbbc79b541b137c811036696a
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-02-12 12:30:12 -0300

    Inclusão de texto

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -2,6 +2,7 @@ import React from "react";
 import './FeaturedMovie.css';
 
 export default ({item}) => {
+  console.log(item);
   return (
     <section className="featured" style={{
       backgroundSize: 'cover',
@@ -10,7 +11,13 @@ export default ({item}) => {
     }}>
       <div className="featured--vertical">
         <div className="featured--horizontal">
-          
+          <div className="featured--name">{item.original_name}</div>
+          <div className="featured--info">
+            <div className="featured--points">{item.vote_average} pontos</div>
+            <div className="featured--year">2099</div>
+            <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+          </div>
+          <div className="featured--descriptio">{item.overview}</div>
         </div>
       </div>
     </section>
