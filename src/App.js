406ce227fb38a857ae90f68d0b5fffc0b8b3f72a
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-02-12 10:21:36 -0300

    Inclusão de texto

--- src/App.js
+++ src/App.js
@@ -7,14 +7,21 @@ import FeaturedMovie from './components/FeaturedMovie';
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
-  const [featuredData, setFeaturedData] = useState(null);
+  const [featuredData, setFeaturedData] = useState([]);
 
   useEffect(() => {
     const loadAll = async () => {
 
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+
+      console.log(chosen);
     }
+    
 
     loadAll();
   }, []);
