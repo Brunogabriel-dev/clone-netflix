a246342c1e195cd798832d6507df612285fe1c79
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-02-12 12:51:19 -0300

    Inclusão de texto

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,6 +3,9 @@ import './FeaturedMovie.css';
 
 export default ({item}) => {
   console.log(item);
+
+  let firstDate = new Date(item.first_air_date);
+
   return (
     <section className="featured" style={{
       backgroundSize: 'cover',
@@ -14,10 +17,14 @@ export default ({item}) => {
           <div className="featured--name">{item.original_name}</div>
           <div className="featured--info">
             <div className="featured--points">{item.vote_average} pontos</div>
-            <div className="featured--year">2099</div>
+            <div className="featured--year">{firstDate.getFullYear()}</div>
             <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
           </div>
           <div className="featured--descriptio">{item.overview}</div>
+          <div className="featured--buttons">
+
+          </div>
+          <div className="featured--genres"><strong>Gêneros:</strong> ...</div>
         </div>
       </div>
     </section>
